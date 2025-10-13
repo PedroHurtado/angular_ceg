7f3ca6af46d79191fee30cc701e4195ee224e595
Pedro Hurtado <pedrohurt@gmail.com>
2025-10-13 13:37:41 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  name="Pedro Hurtado"
+  data:number[]=[]
+  getUser(){
+    /*
+      tenga cierta complejidad
+      use and 'role' in admin
+    */
+    //return "user"
+  }
+  handlerClick(ev:Event){
+    ev.stopPropagation()
+    this.data.push(this.data.length+1)
+  }
 }
