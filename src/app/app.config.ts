2889f875753478565e9a1420b5c62085fb4095b4
Pedro Hurtado <pedrohurt@gmail.com>
2025-10-16 14:00:43 +0200

    pizzaservice

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { Service } from './services/service';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BASE_URL } from './base_url';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide:BASE_URL,
+      useValue:'http://localhost:3000'
+    },
     Service,
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
--- src/app/pizzas/pizzaservice.ts
+++ src/app/pizzas/pizzaservice.ts
@@ -1,15 +1,19 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Inject, Injectable } from '@angular/core';
 import { Pizza } from './pizza';
 import { lastValueFrom } from 'rxjs';
+import { BASE_URL } from '../base_url';
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
-  constructor(private http: HttpClient) {
 
-  }
-  getAll(): Promise<Pizza[]> {
-    return lastValueFrom(this.http.get<Pizza[]>('http://localhost:3000/pizzas'))
+  protected base_url=inject(BASE_URL)
+  protected http=inject(HttpClient)
+  protected path = '/pizzas'
+
+  async getAll(): Promise<Pizza[]> {
+    const pizzas = await lastValueFrom(this.http.get<Pizza[]>(`${this.base_url}${this.path}`))
+    return pizzas
   }
 }
